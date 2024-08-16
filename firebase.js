c30c8ac7a4eb6d616960001d7ba4b4f10f1994d5
Dothat65 <meemoahmed2004@gmail.com>
2024-08-16 00:34:45 -0400

    setup auth functionality

--- firebase.js
+++ firebase.js
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth';
-
-
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
- apiKey: process.env.VITE_APP_API_KEY,
- authDomain: process.env.VITE_APP_AUTH_DOMAIN,
- projectId: process.env.VITE_APP_PROJECT_ID,
- storageBucket: process.env.VITE_APP_STORAGE_BUCKET,
- messagingSenderId: process.env.VITE_APP_MESSAGING_SENDER_ID,
- appId: process.env.VITE_APP_APP_ID,
- measurementId: process.env.VITE_APP_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const firestore = getFirestore(app);
 export const auth = getAuth(app);
-
-export { firestore };
