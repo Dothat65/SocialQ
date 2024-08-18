4898751de4fcd0717c6e099820a42fc8768ed931
Dothat65 <meemoahmed2004@gmail.com>
2024-08-18 16:13:49 -0400

    added functionality to auth page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 'use client'
 import React from 'react';
 import Flashcard, { flashcard } from './components/flashcard';
-import {Login} from './routes/login.jsx';
+import Login from './routes/auth.jsx';
 
 export default function Home() {
   return (
     <div>
-        <Flashcard></Flashcard>
+      <Login />
+
     </div>
   );
 }
